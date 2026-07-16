5c67c247804bce51b10e7935bd8720918863cb31
Christian Peraza <christianperaza2000@gmail.com>
2026-07-15 22:44:32 -0300

    Connect to database

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 // Import dependencies
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv/config';
 // Import index routes
 import indexRoutes from './routes/indexRoutes.js';
+// Import database
+import { connectDB } from './config/database.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors()); // Let our API comunicates with other websites
 app.use(express.json()); // Translate JSON format to JavaScript
+
+// Connect to database
+connectDB();
 
 // Use index routes
 app.use('/', indexRoutes);
