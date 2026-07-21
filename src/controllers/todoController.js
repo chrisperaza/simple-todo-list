88d8d55305d01a038a1a23b328f63f70b77e3af9
Christian Peraza <christianperaza2000@gmail.com>
2026-07-20 22:11:42 -0300

    Add update function

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -103,3 +103,59 @@ export const getTodoById = async (req, res) => {
     });
   }
 };
+
+// ******************************
+// **** UPDATE TO-DO (PUT) ****
+// ******************************
+export const updateTodo = async (req, res) => {
+  try {
+    // Get id from route params
+    const { id } = req.params;
+    // Get data from user
+    const { title, description } = req.body;
+
+    // Validate id with Mongoose
+    if (!isValidObjectId(id)) {
+      return res.status(400).json({
+        success: false,
+        message: 'Invalid To-do ID',
+      });
+    }
+
+    // If title is empty
+    if (!title || title.trim() === '') {
+      return res.status(400).json({
+        success: false,
+        message: 'Title is required',
+      });
+    }
+
+    // Update to-do
+    const todo = await Todo.findByIdAndUpdate(
+      id, // Item to update
+      { title, description }, // Data to update
+      { new: true, runValidators: true }, // Options: 'new' return the updated item, and 'runValidators' runs schema validators
+    );
+
+    // If todo not found
+    if (!todo) {
+      return res.status(404).json({
+        success: false,
+        message: 'To-do not found',
+      });
+    }
+
+    // If todo found and updated
+    return res.status(200).json({
+      success: true,
+      message: 'To-do updated successfully',
+      data: todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+      error: error.message,
+    });
+  }
+};
