249ac71909fdc4d092c04e74673802da3fd9d257
Christian Peraza <christianperaza2000@gmail.com>
2026-07-21 19:41:07 -0300

    Import toggle function

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -6,6 +6,7 @@ import {
   getAllTodos,
   getTodoById,
   updateTodo,
+  toggleTodo,
 } from '../controllers/todoController.js';
 
 const router = express.Router();
@@ -29,6 +30,11 @@ router.get('/:id', getTodoById);
 // **** UPDATE TO-DO (PUT) ****
 // ******************************
 router.put('/:id', updateTodo);
+
+// ******************************
+// **** TOGGLE TO-DO (PATCH) ****
+// ******************************
+router.patch('/:id/toggle', toggleTodo);
 
 // Export router
 export default router;
